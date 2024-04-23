e43b931d3a79de1d8564de55d5a1879270259316
phtech-andyherrmann <andy.herrmann@phtech.com>
2024-04-22 20:56:11 -0700

    Add information to layout

--- app/src/pages/IndexPage.vue
+++ app/src/pages/IndexPage.vue
@@ -1,7 +1,27 @@
 <template>
-  <q-page class="q-pa-xl">
-    <LoaderComponent />
-    <ResultsViewer />
+  <q-page>
+    <div class="q-px-xl">
+      <h6>Upload Your Data
+        <q-icon class="q-ml-sm" name="info">
+          <q-tooltip anchor="top right" self="top right" class="bg-white text-black">
+            <q-banner class="bg-orange text-white q-mt-md">
+              None of your data will be uploaded to any servers. This is only running on your computer.
+            </q-banner>
+            <q-banner :class="$q.dark.isActive ? 'bg-grey-9' : 'bg-grey-2'" class="q-my-md" >
+              <p>Download your usage for the past year from the PGE website and upload into the tool. </p>
+              <p>
+                Doing so will tell you your utilization costs at the current PGE rate and will also estimate your costs if
+                you
+                switch to Time of Day billing.
+              </p>
+            </q-banner>
+          </q-tooltip>
+        </q-icon>
+      </h6>
+
+      <LoaderComponent />
+      <ResultsViewer />
+    </div>
   </q-page>
 </template>
 
